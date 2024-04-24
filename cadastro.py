f33e6143395c0eb557114441219041d5f26b61dd
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-04-24 09:11:08 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,25 @@
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+sexo = input("Digite seu sexo:")
+
+nome2 = input("Digite seu nome 2:")
+idade2 = input("Digite sua idade 2:")
+sexo2= input("Digite seu sexo:")
+
+nome3 = input("Digite seu nome 3:")
+idade3 = input("Digite sua idade 3:")
+sexo3 = input("Digite seu sexo 3:")
+
+print("nome:", nome)
+print("idade:", idade)
+print("sexo:", sexo)
+
+
+print("nome 2:", nome2)
+print("idade 2:", idade2)
+print("sexo 2:", sexo2)
+
+
+print("nome 3:", nome3)
+print("idade 3:", idade3)
+print("sexo 3:", sexo3)
