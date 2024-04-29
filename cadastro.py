218b3af588cf1b4f500ac2ee5cb1c7da5d79e54a
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-04-29 12:34:39 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,25 +1,44 @@
 nome = input("Digite seu nome:")
-idade = input("Digite sua idade:")
+idade = int(input("Digite sua idade:"))
 sexo = input("Digite seu sexo:")
 
 nome2 = input("Digite seu nome 2:")
-idade2 = input("Digite sua idade 2:")
+idade2 = int(input("Digite sua idade 2:"))
 sexo2= input("Digite seu sexo:")
 
 nome3 = input("Digite seu nome 3:")
-idade3 = input("Digite sua idade 3:")
+idade3 = int(input("Digite sua idade 3:"))
 sexo3 = input("Digite seu sexo 3:")
 
 print("nome:", nome)
 print("idade:", idade)
+if (idade2 >= 18):
+    print("maior de idade", idade)
+else: 
+    print("menor de idade", idade)
+
+print("sexo 2:", sexo2)
 print("sexo:", sexo)
 
 
 print("nome 2:", nome2)
 print("idade 2:", idade2)
+
+if (idade2 >= 18):
+    print("maior de idade", idade2)
+else: 
+    print("menor de idade", idade2)
+
 print("sexo 2:", sexo2)
+
 
 
 print("nome 3:", nome3)
 print("idade 3:", idade3)
+
+if (idade3 >= 18):
+    print("maior de idade", idade3)
+else: 
+    print("menor de idade",idade3)
+    
 print("sexo 3:", sexo3)
