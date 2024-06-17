93c613de140ba9a34fc9267012ffd4041c54f978
paulinPC7 <129951094+paulinPC7@users.noreply.github.com>
2024-06-17 08:53:24 -0300

    Create listaf.py

---
+++ listaf.py
@@ -0,0 +1,11 @@
+listafrutas=["morango", "manga", "mamão"]
+fruta="morango"
+print(listafrutas[2])
+print(fruta[0])
+print(listafrutas[2][0])
+listafrutas.append("abacaxi")
+print(listafrutas)
+listafrutas.remove("mamão")
+print(listafrutas)
+listafrutas.sort()
+print(listafrutas)
